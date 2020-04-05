efc9fe7825aab7467cbc2a946afed09fb61b2f73
Tao Ong <taoong@berkeley.edu>
2020-04-05 10:51:18 -0700

    Add tenet card border colors on hover.

--- src/components/TreePage/TreePage.tsx
+++ src/components/TreePage/TreePage.tsx
@@ -17,31 +17,31 @@ export class TreePage extends Component<{}, TreePageState> {
         <h1 className="title">What will you grow today?</h1>
         <div className="card-container">
           <Link to="/activities/compassion" style={{ textDecoration: 'none' }}>
-            <div className="card">
+            <div className="card" id="compassion-card">
               <img className="card-image" src="../../images/tree.png" alt="" />
               <h3 className="card-title">Compassion</h3>
             </div>
           </Link>
           <Link to="/activities/gratitude" style={{ textDecoration: 'none' }}>
-            <div className="card">
+            <div className="card" id="gratitude-card">
               <img className="card-image" src="../../images/tree.png" alt="" />
               <h3 className="card-title">Gratitude</h3>
             </div>
           </Link>
           <Link to="/activities/kindness" style={{ textDecoration: 'none' }}>
-            <div className="card">
+            <div className="card" id="kindness-card">
               <img className="card-image" src="../../images/tree.png" alt="" />
               <h3 className="card-title">Kindness</h3>
             </div>
           </Link>
           <Link to="/activities/mindfulness" style={{ textDecoration: 'none' }}>
-            <div className="card">
+            <div className="card" id="mindfulness-card">
               <img className="card-image" src="../../images/tree.png" alt="" />
               <h3 className="card-title">Mindfulness</h3>
             </div>
           </Link>
           <Link to="/activities/resilience" style={{ textDecoration: 'none' }}>
-            <div className="card">
+            <div className="card" id="resilience-card">
               <img className="card-image" src="../../images/tree.png" alt="" />
               <h3 className="card-title">Resilience</h3>
             </div>
