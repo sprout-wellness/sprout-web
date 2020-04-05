c75af0fa99bf4277d9d299e92f5e4eaa27fe9a4c
Michael <42686036+myou2k@users.noreply.github.com>
2020-04-04 17:18:49 -0700

    Auth WIP

---
+++ src/components/AuthPage/AuthPage.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Router } from 
+import './AuthPage.scss'
+
+import SignIn from './SignIn'
+import SignUp from './SignUp'
+
+function AuthPage() {
+    const user = null;
+    return (
+        user ?
+
+    )
+}
