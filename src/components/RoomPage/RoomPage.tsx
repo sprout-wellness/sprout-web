484d06f3b3001bd11213a07e23eaa6a2a02ff295
Orkun Duman <orkun1675@hotmail.com>
2020-04-04 17:56:14 -0700

    Navigating to room page creates new room with random UUID.

--- src/components/RoomPage/RoomPage.tsx
+++ src/components/RoomPage/RoomPage.tsx
@@ -1,10 +1,27 @@
 import React, { Component } from "react"; // let's also import Component
 import "./RoomPage.scss";
 
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import * as firebase from "firebase/app";
+import "firebase/firestore";
+import { v4 as uuidv4 } from "uuid";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBgKAC5vrSSFfZsCxi1wpVdwk3ZbNlYuIo",
+  authDomain: "sprout-wellness.firebaseapp.com",
+  databaseURL: "https://sprout-wellness.firebaseio.com",
+  projectId: "sprout-wellness",
+  storageBucket: "sprout-wellness.appspot.com",
+  messagingSenderId: "658431260118",
+  appId: "1:658431260118:web:9513c73b085c8eadd22077",
+  measurementId: "G-MYV1VZM8G9"
+};
+
 // the clock's state has one field: The current time, based upon the
 // JavaScript class Date
 type RoomPageState = {
-  id: null;
+  id: String;
   time: Date;
 };
 
@@ -12,22 +29,29 @@ type RoomPageState = {
 // The generic parameters in the Component typing allow to pass props
 // and state. Since we don't have props, we pass an empty object.
 export class RoomPage extends Component<{}, RoomPageState> {
-  // The tick function sets the current state. TypeScript will let us know
-  // which ones we are allowed to set.
+  state = {
+    id: "Creating new room...",
+    time: new Date(),
+  };
+  
   tick() {
     this.setState({
       time: new Date(),
     });
   }
 
-  // Before the component mounts, we initialise our state
-  componentWillMount() {
-    this.tick();
-  }
-
   // After the component did mount, we set the state each second.
   componentDidMount() {
     setInterval(() => this.tick(), 1000);
+    var random_id = uuidv4();
+    firebase.initializeApp(firebaseConfig).firestore().collection('rooms').add({
+      id: random_id,
+    }).catch(function(error) {
+      console.error('Error writing new room to database.', error);
+    });
+    this.setState({
+      id: random_id,
+    });
   }
 
   // render will know everything!
@@ -38,6 +62,8 @@ export class RoomPage extends Component<{}, RoomPageState> {
           Room
         </h1>
         <p>Time: {this.state.time.toLocaleTimeString()}</p>
+        <p>Room ID: {this.state.id}</p>
+        <p>Activity: ABC</p>
       </div>
     );
   }
