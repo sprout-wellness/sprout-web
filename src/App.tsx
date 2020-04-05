1ee5ab578e7e361332b58713d48daa8456d7d1ea
Orkun Duman <orkun1675@hotmail.com>
2020-04-05 15:41:32 -0700

    Add 404 page.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { TreePage } from './components/TreePage/TreePage';
 import { ActivityPage } from './components/ActivityPage/ActivityPage';
 import { RoomPage } from './components/RoomPage/RoomPage';
+import { NotFoundPage } from './components/NotFoundPage/NotFoundPage';
 
 export const APP = function App() {
   return (
@@ -13,6 +14,7 @@ export const APP = function App() {
           <Route exact path="/" component={TreePage} />
           <Route exact path="/activities/:tenet" component={ActivityPage} />
           <Route exact path="/room/:id" component={RoomPage} />
+          <Route component={NotFoundPage} />
         </Switch>
       </div>
     </Router>
