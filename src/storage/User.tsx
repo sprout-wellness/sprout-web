510354251dd9fe8bdaebb83e027b33857803a350
Michael <42686036+myou2k@users.noreply.github.com>
2020-04-12 15:53:41 -0700

    Create and save methods in User.tsx

--- src/storage/User.tsx
+++ src/storage/User.tsx
@@ -10,6 +10,35 @@ export class User {
     this.name = name;
   }
 
+  private save() {
+    const resultPromise = new Promise<void>((resolve, reject) => {
+      firebase
+        .firestore()
+        .collection('users')
+        .doc(this.id)
+        .set({
+          name: this.name
+        })
+        .then(done => {
+          resolve();
+        })
+        .catch(reason => {
+          console.log(`User ${this.id} could not be created.`, reason);
+          reject();
+        });
+    });
+    return resultPromise;
+  }
+
+  static async Create(id: string, name: string) {
+    const user = new User(
+      id,
+      name
+    );
+    await user.save();
+    return user;
+  }
+
   static Load(id: string): Promise<User | undefined> {
     const resultPromise = new Promise<User | undefined>((resolve, reject) => {
       firebase
